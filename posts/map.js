e36acb2e6eeaeb1414ee14565535ed297d4ad4a0
Shaoshuai Dong <dongsoso@hotmail.com>
2016-03-13 00:42:04 +0800

    First commit for gh-pages

---
+++ posts/map.js
@@ -0,0 +1,15 @@
+let CategoryMap = {
+    javascript: 'JavaScript',
+    nodejs: 'Node.js',
+    web: 'Web',
+    ops: 'OPS',
+    others: 'Others'
+};
+
+let PostsMap = [
+    { id: 0, title: '千里之行', category: 'others' }
+];
+
+let GHAddr = 'https://github.com/Dongss/dogs/blob/master/';
+
+export { CategoryMap, PostsMap,  GHAddr}
