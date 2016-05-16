79d4cf151f156fac718757aa286d82432d6e7029
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2016-05-16 13:15:29 +0800

    Add post 4

--- posts/map.js
+++ posts/map.js
@@ -11,7 +11,8 @@ let CategoryMap = {
 let PostsMap = [
     { id: 0, title: '千里之行', category: 'others', date: 1457873783 },
     { id: 2, title: 'Git 小技巧', category: 'others', date: 1462776305 },
-    { id: 3, title: 'JavaScript 变量作用域、this、闭包', category: 'javascript', date: 1462950925 }
+    { id: 3, title: 'JavaScript 变量作用域、this、闭包', category: 'javascript', date: 1462950925 },
+    { id: 4, title: '测试框架：ava', category: 'nodejs', date: 1463365165 }
 ];
 
 let GHAddr = 'https://github.com/Dongss/dogs/tree/master/';
