89f6f37e1ceafdab38306dde07794c3dc5731321
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2016-05-29 21:31:11 +0800

    Add post 6

--- posts/map.js
+++ posts/map.js
@@ -13,7 +13,8 @@ let PostsMap = [
     { id: 2, title: 'Git 小技巧', category: 'others', date: 1462776305 },
     { id: 3, title: 'JavaScript 变量作用域、this、闭包', category: 'javascript', date: 1462950925 },
     { id: 4, title: '测试框架：ava', category: 'nodejs', date: 1463365165 },
-    { id: 5, title: 'JavaScript Array 常用属性和方法', category: 'javascript', date: 1464072499 }
+    { id: 5, title: 'JavaScript Array 常用属性和方法', category: 'javascript', date: 1464072499 },
+    { id: 6, title: 'JavaScript 继承与原型链', category: 'javascript', date: 1464141196 }
 ];
 
 let GHAddr = 'https://github.com/Dongss/dogs/tree/master/';
