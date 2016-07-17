f52a205102b317d1b11c7a9899fd65a9e3097e10
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2016-07-17 17:40:58 +0800

    Add post 7

--- posts/map.js
+++ posts/map.js
@@ -14,7 +14,8 @@ let PostsMap = [
     { id: 3, title: 'JavaScript 变量作用域、this、闭包', category: 'javascript', date: 1462950925 },
     { id: 4, title: '测试框架：ava', category: 'nodejs', date: 1463365165 },
     { id: 5, title: 'JavaScript Array 常用属性和方法', category: 'javascript', date: 1464072499 },
-    { id: 6, title: 'JavaScript 原型、原型链与继承', category: 'javascript', date: 1464141196 }
+    { id: 6, title: 'JavaScript 原型、原型链与继承', category: 'javascript', date: 1464141196 },
+    { id: 7, title: 'Start Node.Js process with PM2 in a Docker container', category: 'ops', date: 1468747033 }
 ];
 
 let GHAddr = 'https://github.com/Dongss/dogs/tree/master/';
