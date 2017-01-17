dd34315304ebe3112af9515d38d5df2a78e601ab
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2017-01-17 15:02:55 +0800

    add post 11

--- posts/map.js
+++ posts/map.js
@@ -18,7 +18,8 @@ let PostsMap = [
     { id: 7, title: 'Build Node.Js web server in Docker containers: nodejs+pm2+mongodb+redis', category: 'ops', date: 1468747033 },
     { id: 8, title: 'JavaScript Function、函数声明、函数表达式', category: 'javascript', date: 1479877601 },
     { id: 9, title: 'Promise Magic', category: 'javascript', date: 1480585712 },
-    { id: 10, title: 'NPM Magic', category: 'nodejs', date: 1482912684 }
+    { id: 10, title: 'NPM Magic', category: 'nodejs', date: 1482912684 },
+    { id: 11, title: 'Graylog 安装使用', category: 'ops', date: 1484620294 }
 ];
 
 let GHAddr = 'https://github.com/Dongss/dogs/tree/master/';
