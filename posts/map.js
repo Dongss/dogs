a54fba9db1fd8dc1f37c802d44db12b4cefaf30e
Shaoshuai Dong <dongshaoshuai@ucloud.cn>
2017-01-19 18:18:54 +0800

    update post 11 title

--- posts/map.js
+++ posts/map.js
@@ -19,7 +19,7 @@ let PostsMap = [
     { id: 8, title: 'JavaScript Function、函数声明、函数表达式', category: 'javascript', date: 1479877601 },
     { id: 9, title: 'Promise Magic', category: 'javascript', date: 1480585712 },
     { id: 10, title: 'NPM Magic', category: 'nodejs', date: 1482912684 },
-    { id: 11, title: 'Graylog 安装使用', category: 'ops', date: 1484620294 }
+    { id: 11, title: '日志管理工具-Graylog 安装使用', category: 'ops', date: 1484620294 }
 ];
 
 let GHAddr = 'https://github.com/Dongss/dogs/tree/master/';
